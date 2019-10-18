aa3980958c3fb434373709ec4ff3c87f2231ab2f
John Stauffer <jstauffer@homeadvisor.com>
2019-10-18 13:22:11 -0600

    Cors

--- service/lambdas/userCrud.js
+++ service/lambdas/userCrud.js
@@ -3,6 +3,8 @@ const dynamodb = new AWS.DynamoDB({ region: 'us-east-1', apiVersion: '2012-08-10
 
 module.exports.createUser = (event, context, callback) => {
     var registrationJson = JSON.parse(event.body);
+    var user = event.requestContext.authorizer
+    console.log("requestContext: ", event.requestContext);
     const params = {
         Item: {
             "userId": {
@@ -22,7 +24,7 @@ module.exports.createUser = (event, context, callback) => {
             }
         },
         // TODO get this from environment variable
-        TableName: "users-dev"
+        TableName: "users-development"
     };
 
     console.log(params);
@@ -35,6 +37,14 @@ module.exports.createUser = (event, context, callback) => {
         else {
             console.log(params);
             console.log(data);
+            const response = {
+                statusCode: 200,
+                headers: {
+                  "Access-Control-Allow-Origin" : "http://localhost:3000", // Required for CORS support to work
+                  "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+                },
+                body: JSON.stringify(data),
+              };
             callback(null, data);
         }
     });
