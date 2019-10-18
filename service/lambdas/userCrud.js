f9839cc00aefdc6817c13815ca52f92094ffd93c
John Stauffer <jstauffer@homeadvisor.com>
2019-10-18 14:04:17 -0600

    Cors

--- service/lambdas/userCrud.js
+++ service/lambdas/userCrud.js
@@ -40,7 +40,7 @@ module.exports.createUser = (event, context, callback) => {
             const response = {
                 statusCode: 200,
                 headers: {
-                  "Access-Control-Allow-Origin" : "http://localhost:3000", // Required for CORS support to work
+                  "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
                   "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
                 },
                 body: JSON.stringify(data),
