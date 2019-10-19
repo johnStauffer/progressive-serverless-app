545dcd36f34ead822c133430cf5b0ac61c431fbc
dean.gilewicz <dean.gilewicz@homeadvisor.com>
2019-10-18 21:50:40 -0600

    WIP deploy serverless

--- service/lambdas/userCrud.js
+++ service/lambdas/userCrud.js
@@ -1,10 +1,6 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB({ region: 'us-east-1', apiVersion: '2012-08-10' });
 
-// module.exports.getUser = (event, context, callback) => {
-
-// }
-
 module.exports.createUser = (event, context, callback) => {
     const registrationJson = JSON.parse(event.body);
     const claims = event.requestContext.authorizer.claims;
@@ -65,5 +61,32 @@ module.exports.createUser = (event, context, callback) => {
         }
     });
 
-
 };
+
+module.exports.getUser = (event, context, callback) => {
+    const eventBodyJson = JSON.parse(event.body);
+    const claims = event.requestContext.authorizer.claims;
+    const username = claims['cognito:username'];
+    const params = {
+        // userId
+        TableName: "users-development"
+    };
+    dynamodb.getItem(params, function(err, data) {
+        if (err) {
+            console.log('dynamodb error' + err);
+            callback(err);
+        }
+        else {
+            console.log(data);
+            const response = {
+                statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+                    "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+                },
+                body: JSON.stringify(data),
+            };
+            callback(null, response);
+        }
+    });
+}
