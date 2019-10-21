47e067bfbf529bb3119fb4657a8868246a4d0d96
John Stauffer <jstauffer@homeadvisor.com>
2019-10-20 19:32:56 -0600

    User get endpoint

--- service/lambdas/userCrud.js
+++ service/lambdas/userCrud.js
@@ -1,24 +1,61 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB({ region: 'us-east-1', apiVersion: '2012-08-10' });
 
-// module.exports.getUser = (event, context, callback) => {
-
-// }
+module.exports.getUser = (event, context, callback) => {
+    cisp.getUser(cispParams, (err, result) => {
+        if (err) {
+            console.log(err);
+            callback(err);
+        }
+        else {
+            console.log(result);
+            const claims = event.requestContext.authorizer.claims;
+            const userId = claims['cognito:username'];
+            const params = {
+                Key: {
+                    "userId": {
+                        S: userId
+                    }
+                },
+                TableName: "users-development"
+            };
+            dynamodb.getItem(params, function (err, data) {
+                if (err) {
+                    console.log('dynamodb error' + err);
+                    callback(err);
+                }
+                else {
+                    console.log(data);
+                    callback(null,
+                        [
+                            {
+                                firstName: data.Item.firstName.S,
+                                lastName: data.Item.lastName.S,
+                                streetAddress: data.Item.streetAddress.S,
+                                city: data.Item.city.S,
+                                state: data.Item.state.S,
+                                zip: data.Item.zip.S
+                            }]);
+                }
+            });
+        }
+    });
+}
 
 module.exports.createUser = (event, context, callback) => {
     const registrationJson = JSON.parse(event.body);
     const claims = event.requestContext.authorizer.claims;
-    const username = claims['cognito:username'];
+    const userId = claims['cognito:username'];
 
     console.log("event", event);
     console.log("context", context);
-    console.log("username: ", username);
+    console.log("username: ", userId);
     console.log("claims", claims)
 
     const params = {
         Item: {
             "userId": {
-                S: registrationJson.userId
+                S: userId
             },
             "firstName": {
                 S: registrationJson.firstName
